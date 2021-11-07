f34224647daded76b9b4790ed5d4bb309848eb23
Stephan benchemoul <stephanb@moveo.co.il>
2021-11-07 17:42:01 +0200

    blabnla

--- quickstart-react/src/App.js
+++ quickstart-react/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
 import mondaySdk from "monday-sdk-js";
-import "monday-ui-react-core/dist/main.css"
+import "monday-ui-react-core/dist/main.css";
 //Explore more Monday React Components here: https://style.monday.com/
-import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js"
+import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
 
 const monday = mondaySdk();
 
@@ -20,17 +20,23 @@ class App extends React.Component {
 
   componentDidMount() {
     // TODO: set up event listeners
+    monday.listen("settings", (res) => {
+      this.setState({ settings: res.data });
+    });
   }
 
   render() {
-    return <div className="App">
-      {/* <AttentionBox
+    console.log(this.state);
+    return (
+      <div className="App" >
+        {/* <AttentionBox
         title="Hello Monday Apps!"
         text="Let's start building your amazing app, which will change the world!"
         type="success"
       /> */}
-      <h1>helooooo</h1>
-    </div>;
+        <h1>helooooo</h1>
+      </div>
+    );
   }
 }
 

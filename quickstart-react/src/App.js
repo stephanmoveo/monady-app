19c6ed8e0828d2e476c6fee22516a13c4118f673
Stephan benchemoul <stephanb@moveo.co.il>
2021-11-09 16:04:54 +0200

    finish scale

--- quickstart-react/src/App.js
+++ quickstart-react/src/App.js
@@ -4,19 +4,33 @@ import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css";
 import VideoJS from "./components/video-js/VideoJS";
 const monday = mondaySdk();
+monday.setToken("eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjEzMjA0MDgyMCwidWlkIjoyNDUzMjYzNywiaWFkIjoiMjAyMS0xMS0wOFQxMDozNTozOC44NjhaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTEwMjMyOCwicmduIjoidXNlMSJ9.f8Vjl4gHGNrCM1XHZV737XuzmDzLEsPCN_svrRHx1Zk")
 
 export default function App() {
   const [itemId, setitemId] = React.useState(0);
+
   React.useEffect(() => {
-    monday.get("context").then((res) => setitemId(res.data.itemIds[0]));
+    monday
+      .get("context")
+      .then((res) => {
+        console.log(res);
+//.data.itemIds === undefined ? 0 : res.data.itemIds[0]
+        setitemId(res);
+        console.log(itemId)
+      })
+      .catch((res) => console.log(res));
   }, []);
+  React.useEffect(() => {
+    console.log(itemId);  
+  }, [itemId]);
   if (itemId !== 0) {
+    console.log(itemId);
     return (
       <div>
-        <VideoJS itemId={itemId} />
+        <VideoJS itemId={itemId.data.itemId} />
       </div>
     );
   } else {
-    return <div></div>;
+    return <div>heloooo</div>;
   }
 }
--- quickstart-react/src/components/video-js/VideoJS.js
+++ quickstart-react/src/components/video-js/VideoJS.js
@@ -3,10 +3,12 @@ import VideoPlayer from "react-video-js-player";
 import "./Video.css";
 import mondaySdk from "monday-sdk-js";
 const monday = mondaySdk();
-
+String.prototype.replaceAll = function(search, replacement) {
+  var target = this;
+  return target.replace(new RegExp(search, 'g'), replacement);
+};
 const VideoJS = (itemId) => {
   const itemid = parseInt(itemId.itemId);
-  const [videoUrl, setVideoUrl] = React.useState("");
   const [updatesNewArr, setupdatesNewArr] = React.useState([]);
   let updatesArr = [];
   let query =
@@ -16,39 +18,34 @@ const VideoJS = (itemId) => {
     monday
       .api(query)
       .then((res) => {
-        console.log(res.data);
         res.data.items[0].updates.forEach((update) => {
           if (update.assets.length > 0) {
             update.assets.forEach((item) => {
               updatesArr.push(item.url);
-              console.log(updatesArr);
             });
           }
         });
         setupdatesNewArr(updatesArr);
       })
-      .then((res) => console.log(res));
+      .catch((res) => console.log(res));
   }, []);
 
-  if (updatesNewArr.length > 0) {
-    return (
-      <div>
-        {updatesNewArr.map((item, index) => {
-          console.log(item);
-          let tempss = item.split("/").pop().replace("%20", " ");
-
-          return (
-            <div key={index}>
-              <div className="movie-text"> {tempss}</div>
-              <VideoPlayer src={item} playbackRates={[0.5, 1, 2, 3, 4]} />
+  return updatesNewArr.length > 0 ? (
+    <div className='video-container'>
+      {updatesNewArr.map((item, index) => {
+        return (
+          <div key={index}>
+            <div className="movie-text">
+              {item.split("/").pop().replaceAll("%20", " ")}
             </div>
-          );
-        })}
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
+            <VideoPlayer src={item} playbackRates={[0.5, 1, 2, 3, 4]} />
+          </div>
+        );
+      })}
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default VideoJS;

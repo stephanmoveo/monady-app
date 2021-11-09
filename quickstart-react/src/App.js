2e64d3f362bc560734d48e298cc3f9ed6ba3a794
Stephan benchemoul <stephanb@moveo.co.il>
2021-11-09 09:38:37 +0200

    kkk

--- quickstart-react/src/App.js
+++ quickstart-react/src/App.js
@@ -5,46 +5,92 @@ import "monday-ui-react-core/dist/main.css";
 //Explore more Monday React Components here: https://style.monday.com/
 import VideoJS from "./components/video-js/VideoJS";
 const monday = mondaySdk();
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    // Default state
-    this.state = {
-      settings: {},
-      name: "",
-      context: {},
-      events: {},
-      taskColorLabel: "",
-      workingIndex: -1,
-      users: [],
-      bugs: [],
-      loading: false,
-      itemId: 0,
-    };
-  }
+//     // Default state
+//     this.state = {
+//       settings: {},
+//       name: "",
+//       context: {},
+//       events: {},
+//       taskColorLabel: "",
+//       workingIndex: -1,
+//       users: [],
+//       bugs: [],
+//       loading: false,
+//       itemId: 0,
+//     };
+//   }
 
-  componentDidMount() {
+//   componentDidMount() {
+//     monday.listen("context", (res) => {
+//       this.setState({ context: res.data });
+
+//       monday.api(
+//           `query ($boardIds: [Int]) { boards (ids:$boardIds) { name items(limit:1) { name id } } }`,
+//           { variables: { boardIds: this.state.context.boardIds } }
+//         )
+//         .then((res) => {
+//           this.setState({ itemId: res.data.boards[0].items[0].id });
+//         });
+//     });
+//   }
+
+//   render() {
+//     if (this.state.itemId != 0) {
+//       return (
+//         <div className="App">
+//           <VideoJS itemId={this.state.itemId} />
+//         </div>
+//       );
+//     } else {
+
+//       return <div></div>;
+//     }
+//   }
+// }
+
+// export default App;
+
+// import React from 'react'
+
+export default function App() {
+  const [mainState, setmainState] = React.useState({
+    settings: {},
+    name: "",
+    context: {},
+    events: {},
+    taskColorLabel: "",
+    workingIndex: -1,
+    users: [],
+    bugs: [],
+    loading: false,
+    itemId: 0,
+  });
+
+  React.useEffect(() => {
     monday.listen("context", (res) => {
-      this.setState({ context: res.data });
+      setmainState({ context: res.data });
       monday
         .api(
-          `query ($boardIds: [Int]) { boards (ids:$boardIds) { name items(limit:1) { name id } } }`,
-          { variables: { boardIds: this.state.context.boardIds } }
+          `query ($boardIds: [Int]) { boards (ids:$boardIds) { name items(limit:20) { name id } } }`,
+          { variables: { boardIds: mainState.context.boardIds } }
         )
         .then((res) => {
-          this.setState({ itemId: res.data.boards[0].items[0].id });
+          console.log(res.data);
+          setmainState({ itemId: res.data.boards[6].items[0].id });
         });
     });
-  }
-
-  render() {
+  }, []);
+  // if (mainState.itemId != 0) {
     return (
-      <div className="App">
-        <VideoJS itemId={this.state.itemId} />
+      <div>
+        <VideoJS itemId={mainState} />
       </div>
     );
-  }
+  // } else {
+  //   return <div></div>;
+  // }
 }
-
-export default App;
--- quickstart-react/src/components/video-js/VideoJS.js
+++ quickstart-react/src/components/video-js/VideoJS.js
@@ -4,40 +4,41 @@ import MOVIE2 from "../../Assets/PetishiOSrec.mov";
 import MOVIE1 from "../../Assets/League-app screen.mov";
 import "./Video.css";
 //75b9b24ff9645307419f67d700113fdf
+//`query ($boardIds: [Int]) { boards (ids:$boardIds) { name items(limit:1) { name id } } }`,
 
 const VideoJS = (itemId) => {
 
+  const itemid = parseInt(itemId.itemId.itemId);
   const [videoUrl, setVideoUrl] = React.useState();
-  let query =
-  "query { items (ids: ["+itemId+"]) { name, updates{assets{url}} }}";
+  console.log(itemid);
+  let query = `query  ($itemid: Int) { items (ids: [$itemid]) { name, updates{assets{url}} }}`;
   React.useEffect(() => {
-   console.log(itemId);
-    signUpSecond()
-   
-  }, []);
+    console.log(itemid);
+    if (itemid != 0) {
+    signUpSecond()}
+  }, [itemid]);
 
   const signUpSecond = async () => {
     try {
-      const res = await fetch('https://api.monday.com/v2', {
+      const res = await fetch("https://api.monday.com/v2", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization:
             "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjEzMjA0MDgyMCwidWlkIjoyNDUzMjYzNywiaWFkIjoiMjAyMS0xMS0wOFQxMDozNTozOC44NjhaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTEwMjMyOCwicmduIjoidXNlMSJ9.f8Vjl4gHGNrCM1XHZV737XuzmDzLEsPCN_svrRHx1Zk",
         },
-        body: JSON.stringify({ query: query })
-      })
-      const data = await res.json()
-      setVideoUrl(data.data.items[0].updates[0].assets[0].url)
-    }
-    catch (err) {
+        body: JSON.stringify({ query: query }),
+      });
+      const data = await res.json();
+      console.log(data);
+      // setVideoUrl(data.data.items[0].updates[0].assets[0].url)
+    } catch (err) {
       console.log(err);
     }
-  }
-  console.log(videoUrl);
+  };
+  // console.log(videoUrl);
 
-  return  (
-
+  return (
     <div>
       <VideoPlayer src={MOVIE2} playbackRates={[0.5, 2, 3, 4]} />
     </div>
